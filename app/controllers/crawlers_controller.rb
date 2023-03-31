84d36cac87cd18c63631a179e21ce3e227ad5b1f
supersean <sean.davis1289@gmail.com>
2023-03-31 08:56:17 -0400

    adding delete to user crawlers

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -2,13 +2,29 @@ class CrawlersController < ApplicationController
 
   def index
     @display_name = current_user.name.nil? ? current_user.email : current_user.name
-    @new_crawler = Crawler.new
+    @crawler = Crawler.new
   end
 
   def create
-    new_crawler = Crawler.new(crawler_params)
+    new_crawler = Crawler.new(new_crawler_params)
     new_crawler.save
 
+    redirect_to user_crawlers_path(current_user)
+  end
+
+  def edit
+    @crawler = Crawler.find params[:id]
+  end
+
+  def update
+    crawler = Crawler.find params[:id]
+    crawler.update(crawler_params)
+
+    redirect_to user_crawlers_path(current_user)
+  end
+
+  def destroy
+    Crawler.delete params[:id]
     redirect_to user_crawlers_path(current_user)
   end
 
@@ -17,7 +33,13 @@ class CrawlersController < ApplicationController
   def crawler_params
     crawler = params.require(:crawler).permit(:keywords, drivers: [])
     crawler[:drivers] = crawler[:drivers].reject(&:blank?).map { |id| Driver.find id }
+    crawler
+  end
+
+  def new_crawler_params
+    crawler = crawler_params
     crawler[:user] = current_user
     crawler
   end
+
 end
--- test/controllers/crawlers_controller_test.rb
+++ test/controllers/crawlers_controller_test.rb
@@ -12,7 +12,7 @@ class CrawlersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select ".crawlers"
 
-    assert_select ".crawlers li", count: user.crawlers.count
+    assert_select ".crawler", count: user.crawlers.count
   end
 
   test "i can create a crawler" do
@@ -29,5 +29,36 @@ class CrawlersControllerTest < ActionDispatch::IntegrationTest
 
     assert_not_nil user.crawlers.find_by(keywords:)
   end
-  
+
+  test "i can edit a crawler " do
+    user = users(:seansly)
+    sign_in user
+    crawler = user.crawlers.first
+    get edit_user_crawler_path(user, crawler)
+
+    assert_response :success
+
+    crawler.keywords = "something else"
+    patch user_crawler_path(user, crawler),
+          params: { crawler: { id: 3, keywords: crawler.keywords, drivers: crawler.drivers.map(&:id).take(1) } }
+    assert_equal "something else", crawler.keywords
+    assert_equal 1, crawler.drivers.count
+    assert_not_equal crawler.id, 3
+    assert_redirected_to user_crawlers_path(user)
+  end
+
+  test "i can delete a crawler" do
+    user = users(:seansly)
+    sign_in user
+    crawler = user.crawlers.first
+
+    crawler_count_before = user.crawlers.count
+    delete user_crawler_path(user,crawler)
+    assert_equal(crawler_count_before -1, user.crawlers.count)
+
+    assert_redirected_to user_crawlers_path(user)
+  end
+
+
+
 end
